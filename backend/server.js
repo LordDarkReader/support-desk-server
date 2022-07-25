1f4f77b159c7847d4dd0c766c52b97c88deef405
Cezary Polosak <Cezary.Polosak@asseco.pl>
2022-07-25 11:32:26 +0200

    add ticket route

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.get('/', (req, res) => {
     res.status(200).json({message: 'Hello'});
 });
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/tickets', require('./routes/ticketRoutes'));
 
 app.use(errorHandler);
 
